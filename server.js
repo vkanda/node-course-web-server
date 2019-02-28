9d5041b3a7812233a95fcad03716f3b42519d88c
V Kanda <visukanda@yahoo.com>
2019-02-28 05:06:10 +0000

    add project page

--- server.js
+++ server.js
@@ -58,6 +58,11 @@ app.get('/about', (req, res) => {
     }) ;
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', 
+      {pageTitle: 'Project Page'
+    }) ;
+});
 
 app.get('/bad', (req, res) => {
     res.send({
